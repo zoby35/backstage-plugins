026d77b636b0e6ad6ecdc807cbece02f5c2e891c
Scott Rosenberg <scott@terasky.com>
2025-02-24 19:23:17 +0200

    Update sidebar

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -152,7 +152,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
             </Typography>
             
             <SidebarSubmenuItem title="Claims" to="catalog?filters[kind]=component&filters[type]=crossplane-claim" icon={SiKubernetes} />
-            <SidebarSubmenuItem title="XRDs" to="catalog?filters[kind]=api&filters[owners]=group:default/kubernetes-auto-ingested" icon={SiOpenapiinitiative} />
+            <SidebarSubmenuItem title="CRDs" to="catalog?filters[kind]=api&filters[owners]=group:default/kubernetes-auto-ingested" icon={SiOpenapiinitiative} />
           </SidebarSubmenu>
         </SidebarItem>
         
