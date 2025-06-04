0b8a3437b30f7f977a5ff03fa597fd386a426d49
Pierre Théard <pierre.theard@orange.com>
2025-06-04 10:13:40 +0200

    Update headers to reach APIGroupDiscoveryList using v2 and falling back to v2beta1 if not available

--- plugins/crossplane-resources/src/components/CrossplaneResourceGraph.tsx
+++ plugins/crossplane-resources/src/components/CrossplaneResourceGraph.tsx
@@ -152,7 +152,7 @@ const CrossplaneResourceGraph = () => {
                     init: {
                         method: 'GET',
                         headers: {
-                            'Accept': 'application/json;g=apidiscovery.k8s.io;v=v2;as=APIGroupDiscoveryList,application/json',
+                            'Accept': 'application/json;as=APIGroupDiscoveryList;v=v2;g=apidiscovery.k8s.io,application/json;as=APIGroupDiscoveryList;v=v2beta1;g=apidiscovery.k8s.io,application/json',
                         },
                     },
                 });
