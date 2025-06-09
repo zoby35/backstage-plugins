457c06bc4d2e5729d81549dcc89d4a2a795618ff
Scott Rosenberg <scott@terasky.com>
2025-06-09 15:00:57 +0300

    Update devpod allowed IDEs

--- plugins/devpod-plugin/src/types.ts
+++ plugins/devpod-plugin/src/types.ts
@@ -1,19 +1,24 @@
 export enum DevpodIDE {
     VSCODE = 'vscode',
     CLION = 'clion',
+    CODIUM = 'codium',
     CURSOR = 'cursor',
+    DATASPELL = 'dataspell',
     FLEET = 'fleet',
     GOLAND = 'goland',
     INTELLIJ = 'intellij',
     JUPYTERNOTEBOOK = 'jupyternotebook',
     OPENVSCODE = 'openvscode',
     PHPSTORM = 'phpstorm',
+    POSITRON = 'positron',
     PYCHARM = 'pycharm',
     RIDER = 'rider',
+    RSTUDIO = 'rstudio',
     RUBYMINE = 'rubymine',
     RUSTROVER = 'rustrover',
     VSCODE_INSIDERS = 'vscode-insiders',
     WEBSTORM = 'webstorm',
+    ZED = 'zed',
   }
   
   export interface DevpodConfig {
