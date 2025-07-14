3ca0b3654f301386d0ae209fbb808d876327e11e
Scott Rosenberg <scott@terasky.com>
2025-07-14 21:14:50 +0300

    fix naming of legacy table selector component

--- plugins/crossplane-resources/src/components/LegacyCrossplaneResourcesTableSelector.tsx
+++ plugins/crossplane-resources/src/components/LegacyCrossplaneResourcesTableSelector.tsx
@@ -2,7 +2,7 @@ import { useEntity } from '@backstage/plugin-catalog-react';
 import LegacyCrossplaneV1ResourcesTable from './LegacyCrossplaneV1ResourcesTable';
 import LegacyCrossplaneV2ResourcesTable from './LegacyCrossplaneV2ResourcesTable';
 
-const CrossplaneResourcesTableSelector = () => {
+const LegacyCrossplaneResourcesTableSelector = () => {
   const { entity } = useEntity();
   const version = entity?.metadata?.annotations?.['terasky.backstage.io/crossplane-version'];
 
@@ -13,4 +13,4 @@ const CrossplaneResourcesTableSelector = () => {
   return <LegacyCrossplaneV1ResourcesTable />;
 };
 
-export default CrossplaneResourcesTableSelector; 
+export default LegacyCrossplaneResourcesTableSelector; 
